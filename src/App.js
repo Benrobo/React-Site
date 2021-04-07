ea9d6d239c4f5066637fd5b3ee48f4bd10fd3fc2
benrobo <alumonabenaiah71@gmail.com>
2021-04-07 12:52:53 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="ovl">
+          <ion-icon name="logo-react" className="react-icon"></ion-icon>
+          <br/>
+          <h2>Journing Of Learning React</h2>
+          <br/>
+          <div className="button">
+            <button className="btn">Experience</button>
+          </div>
+        </div>
       </header>
     </div>
   );
 }
 
-export default App;
+
+export default App
---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <a href="index" className="brand">
+        React App
+      </a>
+
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar
